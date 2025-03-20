5e832e76b031c870b5f8342796bfa401aecd522f
Vialsiu <D00265675@student.dkit.ie>
2025-03-20 18:53:03 +0000

    Redesigned index.blade.php for posts.

--- storage/framework/views/a205d93300f08a3eb4a381acfcdd090e87862755.php
+++ storage/framework/views/a205d93300f08a3eb4a381acfcdd090e87862755.php
@@ -2,16 +2,18 @@
 
 <?php $__env->startSection('content'); ?>
 <div class="w-4/5 m-auto text-center">
-    <div class="py-15 border-b border-gray-200">
-        <h1 class="text-6xl">
+    <div class="py-24 border-b border-gray-200  pb-10">
+        <div class="bg-cover bg-center w-full" style="background-image: url('images/piece7.jpg')">
+            <h2 class="text-6xl text-white py-20">
             Blog Posts
-        </h1>
+            </h2>
+        </div>
     </div>
 </div>
 
 <?php if(session()->has('message')): ?>
     <div class="w-4/5 m-auto mt-10 pl-2">
-        <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-4">
+        <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-2">
             <?php echo e(session()->get('message')); ?>
 
         </p>
@@ -19,17 +21,17 @@
 <?php endif; ?>
 
 <?php if(Auth::check()): ?>
-    <div class="pt-15 w-4/5 m-auto">
+    <div class="pt-8 w-4/5 m-auto">
         <a 
             href="/blog/create"
-            class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+            class="uppercase bg-gray-300 text-gray-800 text-sm font-bold py-2 px-6 rounded hover:bg-gray-400 transition duration-300 ease-in-out mb-4">
             Create post
         </a>
     </div>
 <?php endif; ?>
 
 <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-6 border-b border-gray-200">
         <div>
             <img src="<?php echo e(asset('images/' . $post->image_path)); ?>" alt="">
         </div>
@@ -49,17 +51,17 @@
 
             </p>
 
-            <a href="/blog/<?php echo e($post->slug); ?>" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+            <a href="/blog/<?php echo e($post->slug); ?>" class="uppercase bg-gray-300 text-gray-800 text-sm font-bold py-2 px-6 rounded hover:bg-gray-400 transition duration-300 ease-in-out mb-4">
                 Keep Reading
             </a>
 
             <?php if(isset(Auth::user()->id) && Auth::user()->id == $post->user_id): ?>
-                <span class="float-right">
-                    <a 
-                        href="/blog/<?php echo e($post->slug); ?>/edit"
-                        class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
-                        Edit
-                    </a>
+            <span class="float-right">
+                <a href="/blog/<?php echo e($post->slug); ?>/edit"
+                   class="px-4 py-2 bg-gray-200 text-gray-800 font-medium rounded-md hover:bg-gray-300 transition duration-200 ease-in-out">
+                    Edit
+                </a>
+            </span>
                 </span>
 
                 <span class="float-right">
@@ -70,7 +72,7 @@
                         <?php echo method_field('delete'); ?>
 
                         <button
-                            class="text-red-500 pr-3"
+                            class="text-red-700 pr-3 font-medium"
                             type="submit">
                             Delete
                         </button>
@@ -83,4 +85,4 @@
 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
 <?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\fedan\Pictures\2nd Year Work\Viva il Painted Voyage\resources\views/blog/index.blade.php ENDPATH**/ ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\fedan\Pictures\2nd Year Work\Paint Across the World\resources\views/blog/index.blade.php ENDPATH**/ ?>
