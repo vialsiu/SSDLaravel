be7b1b71887f8588dfb1b6068100cf90cce47c69
Vialsiu <D00265675@student.dkit.ie>
2025-03-23 19:14:02 +0000

    Tailwind for cards in exploregalleries, new array for links to redirect.

--- resources/views/partials/exploregalleries.blade.php
+++ resources/views/partials/exploregalleries.blade.php
@@ -1,13 +1,36 @@
+@php
+    $galleryLinks = [
+        'https://www.louvre.fr/en',
+        'https://www.metmuseum.org/',
+        'https://www.nationalgallery.org.uk/',
+        'https://www.uffizi.it/en',
+        'https://www.rijksmuseum.nl/en',
+        'https://www.pradomuseum.es/en',
+        'https://www.vangoghmuseum.nl/',
+        'https://www.museodelnovecento.org/en/',
+        'https://www.moma.org/',
+        'https://www.centrepompidou.fr/en',
+        'https://www.tate.org.uk/',
+        'https://www.hermitagemuseum.org/',
+        'https://www.museivaticani.va/',
+        'https://www.guggenheim.org/',
+        'https://www.getty.edu/museum/',
+        'https://www.saatchigallery.com/',
+    ];
+@endphp
+
 <div class="w-4/5 m-auto py-10">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
         @foreach ($galleries as $gallery)
-            <div class="block rounded-lg shadow-md overflow-hidden hover:shadow-xl transition duration-300 ease-in-out">
-                <div class="h-48 bg-cover bg-center" style="background-image: url('{{ asset('images/' . $gallery->image) }}');"></div>
+            <a href="{{ $galleryLinks[$loop->index] ?? '#' }}" target="_blank" rel="noopener noreferrer"
+                class="block rounded-lg shadow-md overflow-hidden hover:shadow-xl transition duration-300 ease-in-out bg-white">
+                <div class="h-48 bg-cover bg-center"
+                    style="background-image: url('{{ asset('images/' . $gallery->image) }}');"></div>
                 <div class="bg-white p-4">
                     <h3 class="text-lg font-semibold text-gray-800">{{ $gallery->title }}</h3>
                     <p class="text-sm text-gray-600">{{ $gallery->description }}</p>
                 </div>
-            </div>
+            </a>
         @endforeach
     </div>
 </div>
--- storage/framework/views/6171017e58e07400b758e8748942770aa9bee524.php
+++ storage/framework/views/6171017e58e07400b758e8748942770aa9bee524.php
@@ -1,13 +1,36 @@
+<?php
+    $galleryLinks = [
+        'https://www.louvre.fr/en',
+        'https://www.metmuseum.org/',
+        'https://www.nationalgallery.org.uk/',
+        'https://www.uffizi.it/en',
+        'https://www.rijksmuseum.nl/en',
+        'https://www.pradomuseum.es/en',
+        'https://www.vangoghmuseum.nl/',
+        'https://www.museodelnovecento.org/en/',
+        'https://www.moma.org/',
+        'https://www.centrepompidou.fr/en',
+        'https://www.tate.org.uk/',
+        'https://www.hermitagemuseum.org/',
+        'https://www.museivaticani.va/',
+        'https://www.guggenheim.org/',
+        'https://www.getty.edu/museum/',
+        'https://www.saatchigallery.com/',
+    ];
+?>
+
 <div class="w-4/5 m-auto py-10">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
         <?php $__currentLoopData = $galleries; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $gallery): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-            <div class="block rounded-lg shadow-md overflow-hidden hover:shadow-xl transition duration-300 ease-in-out">
-                <div class="h-48 bg-cover bg-center" style="background-image: url('<?php echo e(asset('images/' . $gallery->image)); ?>');"></div>
+            <a href="<?php echo e($galleryLinks[$loop->index] ?? '#'); ?>" target="_blank" rel="noopener noreferrer"
+                class="block rounded-lg shadow-md overflow-hidden hover:shadow-xl transition duration-300 ease-in-out bg-white">
+                <div class="h-48 bg-cover bg-center"
+                    style="background-image: url('<?php echo e(asset('images/' . $gallery->image)); ?>');"></div>
                 <div class="bg-white p-4">
                     <h3 class="text-lg font-semibold text-gray-800"><?php echo e($gallery->title); ?></h3>
                     <p class="text-sm text-gray-600"><?php echo e($gallery->description); ?></p>
                 </div>
-            </div>
+            </a>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     </div>
 </div>
